c43b0f318fc974ea9ba0cce0bb4e73307c55f6d5
arthur.salahutdinov <artes_93@mail.ru>
2024-10-03 01:17:22 +0300

    Update service and tests

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -1,4 +1,12 @@
 package ru.netology.service;
 
 public class CashbackHackService {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        if (amount % boundary == 0) {
+            return 0;
+        }
+        return boundary - amount % boundary;
+    }
 }
